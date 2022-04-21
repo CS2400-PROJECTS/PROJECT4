cac31ed36a1379ba8842ad10dc285972cf2fb911
YvetteCarlos <89424585+YvetteCarlos@users.noreply.github.com>
2022-04-20 20:09:57 -0700

    add files
    added files to github

---
+++ Implementation.java
@@ -0,0 +1,23 @@
+
+public class Implementation {
+	
+
+
+
+	public static void main(String[] args) {
+		MaxHeaps heap = new MaxHeaps();
+		heap.insert(100);
+		heap.insert(94);
+		heap.insert(99);
+		heap.insert(77);
+		heap.insert(93);
+		heap.insert(98);
+		heap.insert(61);
+		heap.insert(68);
+		heap.insert(76);
+		
+		heap.print();
+
+	}
+
+}
---
+++ MaxHeaps.java
@@ -0,0 +1,43 @@
+
+public class MaxHeaps {
+	  private int[] theHeap;
+	    private int capacity;
+	    private int pos;
+	    public MaxHeaps() {
+	        
+	        capacity = 10;
+	        theHeap = new int[capacity];
+
+	    }
+	    public void insert(int value){
+	        if(pos > capacity) {
+	            //Increases the capacity
+	        } else {
+	            theHeap[pos++] = value;
+	            
+	            int child = pos - 1;
+	            int parent = child/2;
+	             while(theHeap[parent]< theHeap[child] && parent > 0) {
+	            	 int tmp = theHeap[parent];
+	            	 theHeap[parent] = theHeap[child];
+	            	 theHeap[child]= tmp;
+	            	 
+	            	 child = parent;
+	            	 parent = child/2;
+	            	 
+	            		
+	             }
+	        }
+
+	    }
+	    public void print() {
+	    	
+
+	        for (int i = 0; i < pos; i++) {
+	        System.out.println(theHeap[i] + ",");
+	        
+	    	}
+	    }
+     }
+
+
